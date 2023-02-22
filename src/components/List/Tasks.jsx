f3ccb609272876b951ddb963d1d6ac9769732c73
yusuketakamatsu <yusuketakamatsu@rebase.co.jp>
2023-02-22 19:27:51 +0900

    use Link for task card.

--- src/components/List/Tasks.jsx
+++ src/components/List/Tasks.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Link, Box } from "@chakra-ui/react";
 import { Task } from "./Task";
 
 export function Tasks({
@@ -9,16 +9,19 @@ export function Tasks({
   }
 
   return tasks.map((task) => {
+    const path = `/detail/${task.id}`
     return (
-      <Box mt={4} mx={4} key={task.id}>
-        <Task
-          title={task.title}
-          startAt={task.startAt}
-          durationTime={task.durationTime}
-          meetingPlace={task.meetingPlace}
-          reward={task.reward}
-          tagId={task.tagId}
-        />
+      <Box mt={4} mx={4}>
+        <Link key={task.id} href={path}>
+          <Task
+            title={task.title}
+            startAt={task.startAt}
+            durationTime={task.durationTime}
+            meetingPlace={task.meetingPlace}
+            reward={task.reward}
+            tagId={task.tagId}
+          />
+        </Link>
       </Box>
     );
   });
