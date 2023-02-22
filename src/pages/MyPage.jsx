73e482338bdf47e62db3e146438d4d5fd80d29fd
Yusaku Inoue <yusaku.inoue@rebase.co.jp>
2023-02-22 11:13:08 +0900

    set link to history, profile

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -3,31 +3,16 @@
 import { Switch, Box, FormControl, FormLabel } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useAppContext } from "../context/appContext";
+import { Link } from "react-router-dom";
 
 export default function MyPage() {
-  const [height, setHeight] = useState(window.innerHeight - 205);
-  const {
-    scrollRef,
-    onScroll,
-    scrollToBottom,
-    isOnBottom,
-    unviewedMessageCount,
-  } = useAppContext();
-
-
+  const { scrollRef, onScroll, scrollToBottom, isOnBottom, unviewedMessageCount } = useAppContext();
 
   return (
-      <Box
-        bg="white"
-        p="5"
-        overflow="auto"
-        borderRadius="10px"
-        height={height}
-        onScroll={onScroll}
-        ref={scrollRef}
-      >
-
-        <div>マイページだよ</div>
-      </Box>    
+    <Box bg="white" p="5" overflow="auto" borderRadius="10px" height={window.innerHeight - 205} onScroll={onScroll} ref={scrollRef}>
+      {/* 自治体の人にとっては募集履歴, 参加者にとっては参加履歴 */}
+      <Link to="history">履歴</Link>
+      <Link to="profile">プロフィール編集</Link>
+    </Box>
   );
 }
