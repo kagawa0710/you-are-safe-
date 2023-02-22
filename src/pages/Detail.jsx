68e5053c57f7c8aaa9716b342da975f7a920c41e
naoya yoshikawa <naoya.yoshikawa@rebase.co.jp>
2023-02-22 09:37:43 +0900

    Fix indent

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,17 +1,17 @@
 // 一覧ページ　'detail/{id}'にアクセスしたときに表示されるページ
 
 import {
-    Badge,
-    Box,
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink, Divider, Flex,
-    Heading, Tag,
-    Text,
-    Image, HStack, Button,
+  Badge,
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink, Divider, Flex,
+  Heading, Tag,
+  Text,
+  Image, HStack, Button,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { useAppContext } from "../context/appContext";
+import {useEffect, useState} from "react";
+import {useAppContext} from "../context/appContext";
 
 export default function Detail() {
   const [height, setHeight] = useState(window.innerHeight - 205);
@@ -24,16 +24,15 @@ export default function Detail() {
   } = useAppContext();
 
 
-
   return (
       <Box
-        bg="white"
-        p="5"
-        overflow="auto"
-        borderRadius="10px"
-        // height={height}
-        onScroll={onScroll}
-        ref={scrollRef}
+          bg="white"
+          p="5"
+          overflow="auto"
+          borderRadius="10px"
+          // height={height}
+          onScroll={onScroll}
+          ref={scrollRef}
       >
         <Breadcrumb>
           <BreadcrumbItem>
@@ -45,13 +44,14 @@ export default function Detail() {
           </BreadcrumbItem>
         </Breadcrumb>
 
-        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7br1bzBVg6pxikOK5YVMZA4zHcez6RXXWyg&usqp=CAU" alt="防災訓練" objectFit="cover" height="300px" />
+        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7br1bzBVg6pxikOK5YVMZA4zHcez6RXXWyg&usqp=CAU"
+               alt="防災訓練" objectFit="cover" height="300px"/>
         <Box p="4">
           <Heading size="lg" mb="2">消防訓練のサポーター募集</Heading>
           <HStack spacing={2}>
             {['防災', 'サポータ募集', '昼間'].map((tag) => (
                 <Tag size="sm" key={tag}>
-                    {tag}
+                  {tag}
                 </Tag>
             ))}
           </HStack>
@@ -59,8 +59,9 @@ export default function Detail() {
             <Badge colorScheme="green" mr="2">練馬区 小竹町会</Badge>
             <Text fontSize="sm">作成日: 2022/2/25</Text>
           </Flex>
-          <Text mb="4">説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります。</Text>
-          <Divider my="4" />
+          <Text
+              mb="4">説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります。</Text>
+          <Divider my="4"/>
           <Box>
             <Heading size="md" mb="2">詳細情報</Heading>
             <Box>
@@ -72,14 +73,14 @@ export default function Detail() {
               <Text mb="2"><strong>緊急連絡先:</strong> 080-0000-0000</Text>
             </Box>
           </Box>
-        <Box>
-      </Box>
-    </Box>
-          <Box position="fixed" bottom="0" left="0" width="100%" padding="4">
-              <Button onClick={() => console.log('参加する')} width="100%" size="lg" colorScheme="blue">
-                  参加する
-              </Button>
+          <Box>
           </Box>
-  </Box>
+        </Box>
+        <Box position="fixed" bottom="0" left="0" width="100%" padding="4">
+          <Button onClick={() => console.log('参加する')} width="100%" size="lg" colorScheme="blue">
+            参加する
+          </Button>
+        </Box>
+      </Box>
   );
 }
