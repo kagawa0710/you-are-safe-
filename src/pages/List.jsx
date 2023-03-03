c0fa886c4d7ce0a8d22fcedfdcd848b8e633539c
yusuketakamatsu <yusuketakamatsu@rebase.co.jp>
2023-03-03 14:47:16 +0900

    add line.

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,4 +1,4 @@
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Spacer } from "@chakra-ui/react";
 import { Tasks, Filter, Sort } from "../components/List";
 import tasks from "../../data/tasks";
 
@@ -9,12 +9,15 @@ export default function List() {
         tasks={tasks()}
       />
       <Flex position="sticky" bottom={0} bg="white" p={4} w="full">
+        <Spacer />
         <Box>
           <Filter/>
         </Box>
+        <Spacer />
         <Box ml={4}>
           <Sort/>
         </Box>
+        <Spacer />
       </Flex>
     </>
   );
