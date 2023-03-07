438191065d176ec4b3017243d503d236334a935f
naoya yoshikawa <naoya.yoshikawa@rebase.co.jp>
2023-03-07 18:52:03 +0900

    Designed

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,87 +1,64 @@
 // 一覧ページ　'detail/{id}'にアクセスしたときに表示されるページ
 
-import {
-  Badge,
-  Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink, Divider, Flex,
-  Heading, Tag,
-  Text,
-  Image, HStack, Button,
-} from "@chakra-ui/react";
-import {useEffect, useState} from "react";
+import {Box, Button, Center, Divider, Flex, Heading, HStack, Image, Tag, Text} from "@chakra-ui/react";
 import {useAppContext} from "../context/appContext";
+import {iconSrc} from "../libs/icons"
+import tasks from "../../data/tasks";
+import {useParams, useHistory} from 'react-router-dom';
+import {toMonthAndDayJP} from "../libs/date";
+import mapImage from '../images/map.png'
 
-export default function Detail() {
-  const [height, setHeight] = useState(window.innerHeight - 205);
+const Detail = () => {
   const {
     scrollRef,
     onScroll,
-    scrollToBottom,
-    isOnBottom,
-    unviewedMessageCount,
   } = useAppContext();
-
+  const {id} = useParams();
+  const history = useHistory();
+  const task = tasks().filter(t => t.id === Number(id))[0];
 
   return (
       <Box
           bg="white"
           p="5"
-          pb="24"
+          pb="48"
           overflow="auto"
           borderRadius="10px"
-          // height={height}
           onScroll={onScroll}
           ref={scrollRef}
       >
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbLink href='/'>List</BreadcrumbLink>
-          </BreadcrumbItem>
 
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink href='#'>Detail</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
+        <Heading size="lg" mb="2">{task.title}</Heading>
+        <HStack spacing={2}>
+          {task.tags.map((tag) => (
+              <Tag size="sm" key={tag}>
+                {`#${tag}`}
+              </Tag>
+          ))}
+        </HStack>
+        <Flex mt={2}>
+          <Center minW='44px'>
+            <img src={iconSrc(task.iconId)}/>
+          </Center>
+          <Box ml="8">
+            <Text mb="2">開催日:{toMonthAndDayJP(task.startAt)}</Text>
+            <Text mb="2">場所:{task.address}</Text>
+            <Text mb="2">報酬:￥{task.reward}</Text>
+            <Text mb="2">募集人数:{task.recruitNum}</Text>
+          </Box>
+        </Flex>
+        <Divider my="4"/>
+        <Text mb="4">{task.desc}</Text>
+        <Image src={mapImage} alt="東京都文京区音羽2-12-21" objectFit="cover" height="300px"/>
 
-        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7br1bzBVg6pxikOK5YVMZA4zHcez6RXXWyg&usqp=CAU"
-               alt="防災訓練" objectFit="cover" height="300px"/>
-        <Box p="4">
-          <Heading size="lg" mb="2">消防訓練のサポーター募集</Heading>
-          <HStack spacing={2}>
-            {['防災', 'サポータ募集', '昼間'].map((tag) => (
-                <Tag size="sm" key={tag}>
-                  {tag}
-                </Tag>
-            ))}
-          </HStack>
-          <Flex align="center" mb="4" mt={4}>
-            <Badge colorScheme="green" mr="2">練馬区 小竹町会</Badge>
-            <Text fontSize="sm">作成日: 2022/2/25</Text>
-          </Flex>
-          <Text
-              mb="4">説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります。</Text>
-          <Divider my="4"/>
-          <Box>
-            <Heading size="md" mb="2">詳細情報</Heading>
-            <Box>
-              <Text mb="2"><strong>集合場所:</strong>旭丘中学校</Text>
-              <Text mb="2"><strong>報酬:</strong>1000円</Text>
-              <Text mb="2"><strong>募集人数:</strong>5人</Text>
-              <Text mb="2"><strong>開催日:</strong>11月29日
-                9：00～10：00 </Text>
-              <Text mb="2"><strong>緊急連絡先:</strong> 080-0000-0000</Text>
-            </Box>
-          </Box>
-          <Box>
-          </Box>
-        </Box>
-        <Box position="fixed" bottom="0" left="0" width="100%" padding="4">
-          <Button onClick={() => console.log('参加する')} width="100%" size="lg" colorScheme="blue">
+        <Box position="fixed" bottom="20" left="0" width="100%" padding="4">
+          <Button onClick={() => history.push('/my-page/')} width="100%" size="lg" colorScheme="blue">
             参加する
           </Button>
+          <Center bg="gray.200" mt="2">緊急連絡先:{task.phoneNumber}</Center>
         </Box>
       </Box>
   );
 }
+
+export default Detail;
